f02ec757ecf04c5af25d7fc6fa5fcddbd30d3574
KonstKit <121773261+KonstKit@users.noreply.github.com>
2025-07-27 14:56:38 +0400

    Add PostgreSQL metadata extractor

---
+++ src/main/java/com/dataflow/DataFlowBuilderApplication.java
@@ -0,0 +1,12 @@
+package com.dataflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlowBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFlowBuilderApplication.class, args);
+    }
+}
